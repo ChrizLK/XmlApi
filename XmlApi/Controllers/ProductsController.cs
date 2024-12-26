6fe435ac9289e074718ef6b0d5df9474076219c3
ChrizLK <krishankankanamge@gmail.com>
2024-12-26 12:37:04 +0530

    Add project files.

---
+++ XmlApi/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using XmlApi.Data;
+using XmlApi.Models;
+using static Azure.Core.HttpHeader;
+
+namespace XmlApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public readonly AppDbContext _db;
+       
+        public ProductsController(AppDbContext db)
+        {
+
+            _db = db;
+           
+        }
+
+        [HttpGet]
+        public IActionResult Getall()
+        {
+            var result = _db.Products.ToList();
+            return Ok(result);
+        }
+
+    }
+}
---
+++ XmlApi/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+using XmlApi.Models;
+using static Azure.Core.HttpHeader;
+
+namespace XmlApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
